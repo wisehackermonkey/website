c49d0199579348a5453da33e51a3bb3780f44949
Oran C <oranbusiness@gmail.com>
2021-12-04 15:37:09 -0700

    asdfasd

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ var options = {
 };
 
 
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, "..", 'dist')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,"..", 'dist', 'index.html'));
