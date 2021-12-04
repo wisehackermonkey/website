4db710d0e1d5ceee706d53d8217c8f49f45ee626
Oran C <oranbusiness@gmail.com>
2021-12-04 15:34:13 -0700

    afds

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ var options = {
 app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 
